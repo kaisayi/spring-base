041de0c0dd82344d27d473da63cb36580c1b7ebb
kaisayi <1209086740@qq.com>
2019-09-07 17:57:32 +0800

    spring with junit

--- src/test/java/fun/youzz/test/AccountServiceTest.java
+++ src/test/java/fun/youzz/test/AccountServiceTest.java
@@ -5,27 +5,45 @@ import fun.youzz.domain.Account;
 import fun.youzz.service.IAccountService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 /**
  * 使用Junit单元测试；测试配置
+ *
+ * Spring 集合 junit的配置
+ *     1. 导入spring整合Junit的坐标
+ *     2. 使用junit提供的一个注解把原来的main方法替换掉，替换成spring框架提供的
+ * @Runwith
+ *     3. 告知spring的运行器，spring的ioc创建是基于xml还是注解的，并说明位置
+ * @ContextConfiguration
+ *    locations: 指定xml，加上classpath
+ *    classes：指定注解类的位置
+ *
+ * 使用 spring5.x 版本时，要求junit的jar必须是4.12以上
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class AccountServiceTest {
 
-    private ApplicationContext ac;
+    @Autowired
     private IAccountService accountService;
 
-    @Before
-    public void init() {
-        // 获取容器
-//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        // 得到业务层对象
-        accountService = ac.getBean("accountService", IAccountService.class);
-    }
+//    @Before
+//    public void init() {
+//        // 获取容器
+////        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+//        // 得到业务层对象
+//        accountService = ac.getBean("accountService", IAccountService.class);
+//    }
 
     @Test
     public void testFindAll() {
